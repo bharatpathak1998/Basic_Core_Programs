33a109227f01b7bf244f676a59d9a8c0308dc9bf
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-09 20:56:56 +0530

    Added : To Check Percentage Of Head And Tail

---
+++ FlipCoinMain.java
@@ -0,0 +1,9 @@
+package BCP1;
+
+public class FlipCoinMain {
+    public static void main(String[] args) {
+        System.out.println("Percentage Of Heads And Tails");
+        FlipCoin obj = new FlipCoin();
+        obj.flipCoin();
+    }
+}
